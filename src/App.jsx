d4ae8bb478152e00c68e71503191b8a3e416021b
japh <jenny.scherr@svenskaspel.se>
2020-04-27 20:53:13 +0200

    added basic files, like header and footer to the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import "./App.css";
+// import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <h1 id="header">Rock - Paper - Scissor</h1>
+    </nav>
+  );
+};
+export default Header;
