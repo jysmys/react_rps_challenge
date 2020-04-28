e5458bbb99abb1b813d0841996712e97ac71a1c8
japh <jenny.scherr@svenskaspel.se>
2020-04-28 08:24:01 +0200

    updated cypress test to click on start game

--- cypress/integration/userCanStartGame.spec.js
+++ cypress/integration/userCanStartGame.spec.js
@@ -5,10 +5,8 @@ describe("User starts a game", () => {
 
   it("User can click on Start button", () => {
     cy.get("button#startgame").click();
-  });
-  it("User can start round", () => {
-    cy.get("button#startround").click();
     cy.get("button#startgame").should("not.be.visible");
     cy.get("button#startround").should("be.visible");
+    cy.get("button#startround").click();
   });
 });
