859a38394af0742173706dc9ac36cba95b3df7af
japh <jenny.scherr@svenskaspel.se>
2020-06-05 11:39:31 +0200

    updated the App component to use hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, setState } from "react";
 import Header from "./Header";
 import StartGame from "./components/StartGame";
 import StartRound from "./components/StartRound";
@@ -14,167 +14,138 @@ import "./css/App.css";
 import Winner from "./components/Winner";
 import { Footer } from "./Footer";
 
-class App extends Component {
-  state = {
-    display: false,
-    player: "",
-    computer: "",
-    images: [],
-    currentImg: 0,
-    winner: "",
-    gameWinner: null,
-    playerWins: 0,
-    computerWins: 0,
-  };
+import React from "react";
 
-  componentDidMount() {
-    const imgLink = "./img/rps/";
-    const images = [
-      require(imgLink + "rock-computer.jpg"),
-      require(imgLink + "paper-computer.jpg"),
-      require(imgLink + "scissors-computer.jpg"),
-    ];
-    this.setState({ images: images });
-    this.interval = setInterval(() => this.changeImage(), 1000);
-  }
+const App = () => {
+  const [display, setDisplay] = setState(false);
+  const [player, setPlayer] = setState("");
+  const [computer, setComputer] = setState("");
+  const [counter, setCounter] = setState(null);
+  const [images, setImages] = setState([]);
+  const [currentImg, setCurrentImg] = setState(0);
+  const [winner, setWinner] = setState("");
+  const [gameWinner, setGameWinner] = setState(null);
+  const [playerWins, setPlayerWins] = setState(0);
+  const [computerWins, setComputerWins] = setState(0);
+
+  useEffect(() => {
+    return () => {
+      const imgLink = "./img/rps/";
+      const images = [
+        require(imgLink + "rock-computer.jpg"),
+        require(imgLink + "paper-computer.jpg"),
+        require(imgLink + "scissors-computer.jpg"),
+      ];
+      setImages(images);
+      interval = setInterval(() => changeImage(), 1000);
+    };
+  }, []);
 
   onButtonStartRound = () => {
-    const roundWinner = announceRoundWinner(
-      this.state.player,
-      this.state.computer
-    );
-    const wins = checkGameWinner(
-      roundWinner,
-      this.state.playerWins,
-      this.state.computerWins
-    );
-    this.setState({
-      display: false,
-      counter: this.state.counter,
-      winner: roundWinner,
-      gameWinner: wins.gameWinner,
-      playerWins: wins.playerWins,
-      computerWins: wins.computerWins,
-    });
+    const roundWinner = announceRoundWinner(player, computer);
+    const wins = checkGameWinner(roundWinner, playerWins, computerWins);
+    setDisplay(false);
+    setCounter(counter);
+    setWinner(roundWinner);
+    setGameWinner(wins.gameWinner);
+    setPlayerWins(wins.playerWins);
+    computerWins(wins.computerWins);
   };
-  
+
   onImgPick = (player) => {
     let computer;
     computer = getComputerChoise();
     while (computer === player) {
       computer = getComputerChoise();
     }
-    this.setState({ player: player, computer: computer });
+    setPlayer(player);
+    setComputer(computer);
   };
 
-  componentWillUnmount() {
-    this.interval && clearInterval(this.interval);
-  }
-
-  changeImage() {
-    if (this.state.display) {
+  changeImage = () => {
+    if (display) {
       let newCurrentImg = 0;
-      if (this.state.currentImg !== 2) {
-        newCurrentImg = this.state.currentImg + 1;
+      if (currentImg !== 2) {
+        newCurrentImg = currentImg + 1;
       }
-      this.setState({ currentImg: newCurrentImg });
+      setCurrentImg(newCurrentImg);
     }
-  }
+  };
 
-  render() {
-    const {
-      display,
-      images,
-      currentImg,
-      winner,
-      gameWinner,
-      player,
-      computer,
-      playerWins,
-      computerWins,
-    } = this.state;
-    let imgString = images[currentImg];
-    let renderGame;
-    let roundNr = playerWins + computerWins + 1;
-    switch (true) {
-      case !display && gameWinner === null && winner === "":
-        renderGame = (
-          <StartGame
-            onButtonStartGame={() => this.setState({ display: !display })}
-          />
-        );
-        break;
-      case display && gameWinner === null && winner === "":
-        renderGame = (
-          <div id="showpicks">
-            <ShowPicks
-              id={player}
-              onImgPick={this.onImgPick}
-              display={display}
-            />
-            {player !== "" && (
-              <>
-                <ComputerShuffle imgString={imgString} />
-                <StartRound
-                  onButtonStartRound={this.onButtonStartRound}
-                  winner={winner}
-                  roundNr={roundNr}
-                />
-              </>
-            )}
-          </div>
-        );
-        break;
-      case !display && winner !== "" && gameWinner === null:
-        renderGame = (
-          <>
-            <NextRound
-              winner={winner}
-              player={player}
-              computer={computer}
-              roundNr={roundNr}
-              playerWins={playerWins}
-              computerWins={computerWins}
-              onclick={() =>
-                this.setState({ display: true, winner: "", player: "" })
-              }
-            />
-          </>
-        );
-        break;
-      case !display && gameWinner !== null:
-        renderGame = (
-          <Winner
-            gameWinner={gameWinner}
-            playerWins={playerWins}
-            computerWins={computerWins}
+  resetState = () => {
+    setDisplay(false),
+      setGameWinner(null),
+      setWinner(""),
+      setPlayer(""),
+      setComputer(""),
+      setPlayerWins(0),
+      setComputerWins(0);
+  };
+
+  let imgString = images[currentImg];
+  let renderGame;
+  let roundNr = playerWins + computerWins + 1;
+
+  switch (true) {
+    case !display && gameWinner === null && winner === "":
+      renderGame = <StartGame onButtonStartGame={() => setDisplay(!display)} />;
+      break;
+    case display && gameWinner === null && winner === "":
+      renderGame = (
+        <div id="showpicks">
+          <ShowPicks id={player} onImgPick={onImgPick} display={display} />
+          {player !== "" && (
+            <>
+              <ComputerShuffle imgString={imgString} />
+              <StartRound
+                onButtonStartRound={onButtonStartRound}
+                winner={winner}
+                roundNr={roundNr}
+              />
+            </>
+          )}
+        </div>
+      );
+      break;
+    case !display && winner !== "" && gameWinner === null:
+      renderGame = (
+        <>
+          <NextRound
+            winner={winner}
             player={player}
             computer={computer}
-            onButtonPlayAgain={() => {
-              this.setState({
-                display: false,
-                gameWinner: null,
-                winner: "",
-                player: "",
-                computer: "",
-                playerWins: 0,
-                computerWins: 0,
-              });
-            }}
+            roundNr={roundNr}
+            playerWins={playerWins}
+            computerWins={computerWins}
+            onclick={(() => setDisplay(true), setWinner(""), setPlayer(""))}
           />
-        );
-        break;
-      default:
-    }
-    return (
-      <>
-        <Header />
-        <div centered className="game">
-          {renderGame}
-        </div>
-        <Footer />
-      </>
-    );
+        </>
+      );
+      break;
+    case !display && gameWinner !== null:
+      renderGame = (
+        <Winner
+          gameWinner={gameWinner}
+          playerWins={playerWins}
+          computerWins={computerWins}
+          player={player}
+          computer={computer}
+          onButtonPlayAgain={resetState}
+        />
+      );
+      break;
+    default:
   }
-}
+
+  return (
+    <>
+      <Header />
+      <div centered className="game">
+        {renderGame}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
 export default App;
